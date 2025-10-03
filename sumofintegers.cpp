87ae1cd8940137439e52ff224e5ce2905b80fdd9
Sangini Verma <sanginiv11@gmail.com>
2025-10-03 09:01:09 +0530

    Add files via upload

---
+++ sumofintegers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int funf(int a){
+    if (a==1){
+        return 1;
+    }
+    else{
+        return (a+funf(a-1));
+    }
+}
+int main(){
+    int d;
+    std::cout<<"enter the value ";
+    std::cin>>d;
+    std::cout<<"output is : "<<funf(d);
+
+return 0;
+}
+//OUTPUT
+//enter the value 5
+//output is : 15
